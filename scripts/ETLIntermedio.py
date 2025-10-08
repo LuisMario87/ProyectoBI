7d7005a88ebb9a9ebdbdbcb712907adf9b246d24
LuisMario87 <143245995+LuisMario87@users.noreply.github.com>
2025-10-07 22:04:06 -0600

    Update ETLIntermedio.py

--- scripts/ETLIntermedio.py
+++ scripts/ETLIntermedio.py
@@ -4,7 +4,7 @@ import pandas as pd
 def load_data(path="/mnt/data/demoDatos.xlsx"):
     df = pd.read_excel(path)
     df.columns = df.columns.str.strip()
-    df["FechaCompra_parsed"] = pd.to_datetime(df.get("FechaCompra"), errors="coerce")
+    df["FechaCompra"] = pd.to_datetime(df.get("FechaCompra"), errors="coerce")
     df["Cantidad"] = pd.to_numeric(df.get("Cantidad"), errors="coerce")
     df["PrecioUnitario"] = pd.to_numeric(df.get("PrecioUnitario"), errors="coerce")
     df["TotalVenta"] = df["Cantidad"] * df["PrecioUnitario"]
